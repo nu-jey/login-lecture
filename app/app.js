9f1909a6d425628bca4c951284ef185ecb9bcd12
nu-jey <yp50777@naver.com>
2022-09-16 00:33:32 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/app.js
+++ app/app.js
@@ -5,9 +5,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const accessLogStream = require("./src/config/log");
 dotenv.config();
-
-const PORT = 3000;
 
 // 라우팅   
 const home = require("./src/routes/home");
@@ -19,6 +19,10 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 // url을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함되는 경우 제대로 인식되지 않는 문제 해결 
 app.use(bodyParser.urlencoded({extended : true}));
+
+app.use(morgan('dev')); // 개발시 콘솔로 바로 확인할 수 있도록 
+app.use(morgan('common', { stream: accessLogStream})); // 개발 후에는 tiny정도가 적정 
+
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메소드 
 
